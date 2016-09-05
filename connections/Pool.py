31b37bce6e1ed5eaedd2e8a0c620fbf1840e68a8
lttazz99 <lttazz99@gmail.com>
2016-09-05 20:30:53 +0530

    Moved conn pool to a class and added methods to create connection pools for all datasources.

--- connections/Pool.py
+++ connections/Pool.py
@@ -1,56 +1,86 @@
 from cassandra.cluster import Cluster
-from redis import Redis
-from mysql.connector.connection import MySQLConnection
+from redis import Redis, ConnectionPool
+from mysql.connector.pooling import MySQLConnectionPool, CNX_POOL_MAXSIZE
 from pymongo import MongoClient
 from config.GetConfig import get_configs
 
-CASSANDRA = 'Cassandra'
-REDIS = 'Redis'
-MONGO = 'MongoDB'
-MYSQL = 'MySQL'
-HOST = 'host'
-PORT = 'port'
-USER = 'user'
-PASS = 'pass'
-COMMA = ','
-configs = None
 
+# Pool initialization to be performed in startup script
 
-# perhaps convert to constructor
-def get_config():
-    global configs
-    configs = get_configs()
-    # return config[datasource]
+class Pool:
+    CASSANDRA = 'Cassandra'
+    REDIS = 'Redis'
+    MONGO = 'MongoDB'
+    MYSQL = 'MySQL'
+    HOST = 'host'
+    PORT = 'port'
+    USER = 'user'
+    PASS = 'pass'
+    COMMA = ','
+    configs = None
+    redisPool = None
+    mongoPool = None
+    cassandraPool = None
+    mysqlPool = None
 
+    def __init__(self):
+        global configs
+        configs = get_configs()
+        self.create_cassandra()
+        self.create_redis()
+        self.create_mongo()
+        self.create_mysql()
 
-def get_cassandra():
-    config = configs[CASSANDRA]
-    hosts, port, user, password = get_params(config)
-    hosts = hosts.strip().split(COMMA)
-    cluster = Cluster(hosts, port)
-    return cluster.connect()
+    def get_params(self, config):
+        global HOST, PORT, USER, PASS
+        return config[HOST], config[PORT], config[USER], config[PASS]
 
+    # perform exception handling with logging
 
-def get_redis():
-    config = configs[REDIS]
-    hosts, port, user, password = get_params(config)
-    if password:
-        return Redis(hosts, password=password)
-    else:
-        return Redis(hosts)
+    def create_cassandra(self):
+        global CASSANDRA, COMMA, cassandraPool
+        config = configs[CASSANDRA]
+        hosts, port, user, password = self.get_params(config)
+        hosts = hosts.strip().split(COMMA)
+        cassandraPool = Cluster(hosts, port)
 
+    def create_redis(self):
+        global REDIS, redisPool
+        config = configs[REDIS]
+        hosts, port, user, password = self.get_params(config)
+        redisPool = ConnectionPool(host=hosts, port=port)
 
-def get_mysql(database):
-    config = configs[MYSQL]
-    hosts, port, user, password = get_params(config)
-    return MySQLConnection(user=user, password=password, host=hosts, port=port, database=database)
+    def create_mysql(self, database):
+        global MYSQL, mysqlPool
+        config = configs[MYSQL]
+        hosts, port, user, password = self.get_params(config)
+        dbconfig = {'database': database,
+                    'user': user,
+                    'password': password,
+                    'host': hosts,
+                    'port': port}
+        mysqlPool = MySQLConnectionPool(pool_size=CNX_POOL_MAXSIZE, pool_name='POOL', **dbconfig)
 
+    def create_mongo(self):
+        global MONGO, mongoPool
+        config = configs[MONGO]
+        hosts, port, user, password = self.get_params(config)
+        mongoPool = MongoClient(host=hosts, port=port)
 
-def get_mongo():
-    config = configs[MONGO]
-    hosts, port, user, password = get_params(config)
-    return MongoClient(host=hosts, port=port)
+    def get_redis(self):
+        return Redis(connection_pool=redisPool)
 
+    def get_cassandra(self):
+        return cassandraPool.connect()
 
-def get_params(config):
-    return config[HOST], config[PORT], config[USER], config[PASS]
+    def get_mysql(self):
+        return mysqlPool.get_connection()
+
+    def get_mongo(self):
+        return mongoPool
+
+    def __del__(self):
+        redisPool.disconnect()
+        mongoPool.close()
+        mysqlPool.close()
+        cassandraPool.shutdown()
