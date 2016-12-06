512864208db175cb9c26df695fbf23dde0bba746
lttazz99 <lttazz99@gmail.com>
2016-12-07 00:19:20 +0530

    Added os path expressions to correctly resolve the config file location

--- config/GetConfig.py
+++ config/GetConfig.py
@@ -1,10 +1,11 @@
 import yaml
+import os
 
 
 # Will have to be run on deployment automatically
 
 def get_configs():
-    with open('../resources/config.yaml', 'rb') as f:
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../resources/config.yaml'), 'rb') as f:
         data = yaml.load(f)
         return data
 
